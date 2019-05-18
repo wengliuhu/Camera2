f821615d9f040aac841bc35e93fb5156a893ad07
wengliuhu <424511394@qq.com>
2019-05-18 15:40:51 +0800

    初始化项目

---
+++ app/src/main/java/test/Invocation.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * author: created by wengliuhu
+ * time: 2019/5/13 16
+ */
+public class Invocation implements InvocationHandler
+{
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable
+    {
+        return null;
+    }
+}
